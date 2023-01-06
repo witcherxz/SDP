0c0ca1f37d965311f328d2e815b9685068fc5a02
Abdullah <witcherxzr@hotmail.com>
2023-01-06 17:37:01 +0300

    Update aruco side length

--- RobotVision/opencv_constants.cpp
+++ RobotVision/opencv_constants.cpp
@@ -13,7 +13,7 @@
 namespace constants {
     std::string homedir = std::getenv("HOME");
     extern const float calibrationSquareDimension = 0.025f; // in meter
-    extern const float arucoSquareDimension = 0.14f; // in meter
+    extern const float arucoSquareDimension = 0.2f; // in meter
     extern const cv::Size chessboardDimensions = cv::Size(6, 9);
     extern const int dictionaryName = cv::aruco::DICT_4X4_50;
     extern const std::string cameraCalibrationPath = homedir + "\\Documents\\cameraCalibration";

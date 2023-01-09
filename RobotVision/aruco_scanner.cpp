b5a89ac9d9bec2d078196737108b08358cf41c1e
Abdullah <witcherxzr@hotmail.com>
2023-01-09 13:20:45 +0300

    refactor aruco scanner

--- RobotVision/aruco_scanner.cpp
+++ RobotVision/aruco_scanner.cpp
@@ -7,83 +7,201 @@
 #include "Map/map.h"
 #include "CameraCalibration/camera_calibration.h"
 #include "opencv_constants.h"
-
-void showCamErrorMassage() {
+#include <functional>
+#include "./aruco_scanner.h"
+#include <unordered_map>
+#include <algorithm>
+void ArucoScanner::showCamErrorMassage() {
     std::cout << "Error : Could not open camera";
     exit(1);
 }
 
-void monitorCamera(cv::Mat frame, cv::VideoCapture vid, const std::function<void()> &func) {
-    while (vid.read(frame)) {
-        func();
-    }
+template<typename T>
+std::vector<T> findDiff(std::vector<T> x, std::vector<T> y) {        // no-ref, no-const
+    std::vector<T> diff;
+    std::sort(x.begin(), x.end());
+    std::sort(y.begin(), y.end());
+    std::set_difference(x.begin(), x.end(), y.begin(), y.end(), std::back_inserter(diff));
+    return diff;
 }
 
-void showTranslationInfo(const cv::Mat &frame, const std::vector<cv::Vec3d> &translationVectors, int id) {
-    if (translationVectors.size() > 0) {
+void ArucoScanner::showTransformationInfo(const cv::Mat &frame) {
+    if (translationVectors.size().width > 0) {
         std::ostringstream distance;
-        distance << "ID: " << id << ", distance : " << translationVectors[0];
-        putText(frame, distance.str(), cv::Point2i(10, frame.cols - 200), cv::FONT_HERSHEY_COMPLEX, .7,
+        std::ostringstream rotation;
+        distance << "ID: " << markerIds[0] << ", distance : " << rotationVectors.at<cv::Vec3d>(0);
+        rotation << "rotation : " << rotationVectors.at<cv::Vec3d>(0
+        );
+        putText(frame, distance.str(), cv::Point2i(10, frame.cols - 220), cv::FONT_HERSHEY_COMPLEX, .7,
+                cv::Scalar(0, 255, 0));
+        putText(frame, rotation.str(), cv::Point2i(10, frame.cols - 180), cv::FONT_HERSHEY_COMPLEX, .7,
                 cv::Scalar(0, 255, 0));
     }
 }
-
-// void
-// updatePositionOnMap(int arucoXPos, int arucoYPos, const std::vector<cv::Vec3d> &translationVectors, GridMap &map) {
-//     if (!translationVectors.empty()) {
-//         map.changeCurrentPosition(map.,arucoXPos - translationVectors[0][0], arucoYPos + translationVectors[0][1]);
-//     }
-// }
-
-void drawMarkersOnFrame(const cv::Mat &frame, const cv::Mat &distanceCoefficients, const cv::Mat &cameraMatrix,
-                        const std::vector<int> &markerIds, const std::vector<cv::Vec3d> &rotationVectors,
-                        const std::vector<cv::Vec3d> &translationVectors) {
-    for (int i = 0; i < markerIds.size(); i++) {
-        // ostringstream x;
-        drawFrameAxes(frame, cameraMatrix, distanceCoefficients, rotationVectors[i], translationVectors[i],
-                      constants::arucoSquareDimension);
-    }
-}
-
-void estimateMarkersPose(const cv::Mat frame, const cv::Mat distortionCoefficients, const cv::Mat cameraMatrix, std::vector<int>& markerIds, std::vector<cv::Vec3d> &rotationVectors, std::vector<cv::Vec3d> &translationVectors){
-    cv::Ptr<cv::aruco::Dictionary> markerDictionary = cv::aruco::getPredefinedDictionary(constants::dictionaryName);
-    std::vector<std::vector<cv::Point2f>> markerCorners;
-    cv::aruco::detectMarkers(frame, markerDictionary, markerCorners, markerIds);
-    cv::aruco::estimatePoseSingleMarkers(markerCorners, constants::arucoSquareDimension, cameraMatrix,
-                                             distortionCoefficients, rotationVectors, translationVectors);
-}
-
-void monitorArucoMarkers(cv::Mat frame, const cv::Mat &distortionCoefficients, const cv::Mat &cameraMatrix) {
-    int arucoXPos = 4;
-    int arucoYPos = 6;
-    std::vector<int> markerIds;
-    std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-    std::vector<cv::Vec3d> rotationVectors, translationVectors;
-    cv::Ptr<cv::aruco::Dictionary> markerDictionary = cv::aruco::getPredefinedDictionary(constants::dictionaryName);
+void ArucoScanner::openCamera(std::function<void(cv::Mat&)> func){
     cv::VideoCapture vid(0);
     cv::namedWindow("Cam");
-    if (!vid.isOpened())
-        showCamErrorMassage();
-    GridMap map(30, 10, 0.4);
-
-    // map.drawMap();
+    cv::Mat frame;
+     // Read the image file
+    // cv::imshow("image", image);
+    // cv::Mat image = cv::imread("C:\\Users\\Legend\\Desktop\\mat.jpg");
+    std::cout << "camera created" << std::endl;
+    if (!vid.isOpened()) showCamErrorMassage();
+    vid.set(cv::CAP_PROP_FRAME_WIDTH, 1920);
+    vid.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
     while (vid.read(frame)) {
-        estimateMarkersPose(frame, distortionCoefficients, cameraMatrix, markerIds, rotationVectors, translationVectors);
-        drawMarkersOnFrame(frame, distortionCoefficients, cameraMatrix, markerIds, rotationVectors, translationVectors);
-        // updatePositionOnMap(arucoXPos, arucoYPos, translationVectors, map);
-        if(markerIds.size() != 0){
-        std::cout << "Theta X:" << rotationVectors[0] << std::endl;
-        showTranslationInfo(frame, translationVectors, markerIds[0]);
-            
-        } 
-        imshow("Cam", frame);
+        func(frame);
+        // func(image);
+        
+    imshow("Cam", frame);
         if (cv::waitKey(10) > 0) break;
+    } 
+}
+void ArucoScanner::drawArucoMarker(cv::Mat& frame){
+    for (int i = 0; i < markerIds.size(); i++) {
+        drawFrameAxes(frame, cameraMatrix, distortionCoefficients, rotationVectors.at<cv::Vec3d>(i), translationVectors.at<cv::Vec3d>(i),
+        constants::arucoSquareDimension);
+        showTransformationInfo(frame);
+    }
+
+}
+
+
+void ArucoScanner::estimateMarkersPose(const cv::Mat frame){
+    // Set coordinate system
+    cv::Mat objPoints(4, 1, CV_32FC3);
+
+    const float markerLength = constants::arucoSquareDimension;
+    objPoints.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-markerLength/2.f, markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(markerLength/2.f, markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(markerLength/2.f, -markerLength/2.f, 0);
+    objPoints.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
+    arucoDetector.detectMarkers(frame, markerCorners, markerIds);
+    // std::vector<int> disappearMarkers= findDiff<int>(markerIds, oldDetectedMarker);
+    // for(auto marker : disappearMarkers){
+    //     rotationVectors.erase(marker);
+    //     translationVectors.erase(marker);
+    // }
+    int numberOfDetectedMarker = markerIds.size();
+    if(numberOfDetectedMarker > 0){
+        // // std::vector<std::vector<cv::Point2f>> corners;
+            cv::Mat rvecs, tvecs;
+            rvecs.create(numberOfDetectedMarker, 1, CV_64FC3);
+            tvecs.create(numberOfDetectedMarker, 1, CV_64FC3);
+            // cv::aruco::estimatePoseSingleMarkers(markerCorners, constants::arucoSquareDimension, cameraMatrix, distortionCoefficients, rvecs, tvecs);
+            rotationVectors = rvecs;
+            translationVectors = tvecs;
+            std::cout << tvecs << std::endl;
+        
+        for (int i = 0; i < markerIds.size(); i++) {
+            int id = markerIds[i];
+            
+            // bool markerRecordExist = translationVectors.count(id) > 0;
+            cv::solvePnP(objPoints, markerCorners[i], cameraMatrix, distortionCoefficients, rvecs.at<cv::Vec3d>(i), tvecs.at<cv::Vec3d>(i), false);
+            
+            // for (int i = 0; i < rvecs.size().width; i++){
+            //     std::cout << rvecs.at<cv::Vec3d>(i)[0] << std::endl;
+            // }
+            
+            // cv::OutputArray
+            // std::cout << "ADD rot called with " << rvecs.clone() << " as an input" << std::endl;
+            // rotVecAvg.add(rvecs.clone());
+            // std::cout << "ADD trans called with " << tvecs.clone() << " as an input" << std::endl;
+            // tranVecAvg.add(tvecs.clone());
+            // rotationVectors = rotVecAvg.getAverage();
+            rotationVectors = rvecs;
+            translationVectors = tvecs;
+            // std::cout << rotationVectors << std::endl;
+
+            // if(markerRecordExist){
+            //     // cv::solvePnPRefineLM(objPoints, markerCorners[i], cameraMatrix, distortionCoefficients, rotationVectors[i], translationVectors[i]);
+            //     cv::solvePnPRefineVVS(objPoints, markerCorners[i], cameraMatrix, distortionCoefficients, rotationVectors[i], translationVectors[i]);
+            //     // cv::solvePnPRansac(objPoints, markerCorners[i], cameraMatrix, distortionCoefficients, rotationVectors[i], translationVectors[i], true, 100);
+            //     // cv::solvePnP(objPoints, markerCorners[i], cameraMatrix, distortionCoefficients, rotationVectors[i], translationVectors[i], true);
+            // }else{
+            //     cv::solvePnP(objPoints, markerCorners[i], cameraMatrix, distortionCoefficients, rotationVectors[i], translationVectors[i]);
+            // }
+        }
+        idsVectorSize = markerIds.size();
     }
 }
 
-void startCamMonitoring(std::string cameraCalibrationPath) {
-    cv::Mat cameraMatrix, distortionCoefficients;
-    cv::Mat frame;
-    loadCameraCalibration(cameraCalibrationPath, cameraMatrix, distortionCoefficients);
-    monitorArucoMarkers(frame, distortionCoefficients, cameraMatrix);
+void ArucoScanner::monitorArucoMarkers(){
+    openCamera(arucoMarkerDrawProccess);
+}
+
+ArucoScanner::vecs3d ArucoScanner::getTranslationVectors(){
+    return translationVectors;
+}
+ArucoScanner::vecs3d ArucoScanner::getRotationVectors(){
+    return rotationVectors;
+}
+
+ArucoScanner::ArucoScanner(){
+    loadCameraCalibration(constants::cameraCalibrationPath, cameraMatrix, distortionCoefficients);
+    cv::Ptr<cv::aruco::Dictionary> markerDictionary = cv::aruco::getPredefinedDictionary(constants::dictionaryName);
+    // const cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters();
+    arucoDetector = cv::aruco::ArucoDetector(markerDictionary);
+
+}
+
+AverageFilter::AverageFilter(){
+    this->bufferSize = 20;
+    // this->buffer.resize(bufferSize);
+    // this->valBuffer.resize(bufferSize);
+}
+
+void AverageFilter::add(cv::Mat pose){
+    buffer.insert(buffer.begin(), pose);
+    valBuffer.insert(valBuffer.begin(), pose.at<cv::Vec3d>(0)[0]);
+    std::cout << "---------------------------------------------" << std::endl;
+    for (int i = 0; i < buffer.size(); i++)
+    {
+        std::cout << buffer[i] << std::endl;
+    }
+    std::cout << "---------------------------------------------" << std::endl;
+    
+    if (valBuffer.size() > bufferSize){
+        buffer.pop_back();
+        valBuffer.pop_back();
+    }
+    std::cout << "SIZE :" << buffer.size() << std::endl;
+    average = cv::Mat::zeros(pose.size(), CV_64FC3);
+    int max = mostFrequentElement(valBuffer);
+    if(buffer[max].at<cv::Vec3d>(0)[0] == 0){
+        std::cout << "max : " << max << std::endl;
+        std::cout << "valBuf(max) : " << valBuffer[max] << std::endl;
+        // std::cout << "buf(19) : " << buffer[19] << std::endl;
+        std::cout << "buf(max) : " << buffer[max] << std::endl;
+            std::cout << "---------------------------------------------" << std::endl;
+            // for (int i = 0; i < buffer.size(); i++)
+            // {
+            //     std::cout << buffer[i] << std::endl;
+            // }
+    }
+    average = buffer[max];
+}
+cv::Mat AverageFilter::getAverage(){
+    return average;
+}
+
+int AverageFilter::mostFrequentElement(const std::vector<double> vec){
+    int max = 0;
+    int maxCount = 0;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        double value = vec[i];
+        int counter = 0;
+        for (int j = 0; j < vec.size(); j++)
+        {
+            if(value == vec[j]){
+                counter++;
+            }
+        }
+        if(counter > maxCount){
+            max = i;
+        }
+    }
+    return max;
+    
 }
--- RobotVision/aruco_scanner.h
+++ RobotVision/aruco_scanner.h
@@ -1,12 +1,78 @@
-#include <opencv2/core/mat.hpp>
 
 #if !defined(SDP_ROBOTVISION_ARUCOPOSEESTEMATION_H)
 #define SDP_ROBOTVISION_ARUCOPOSEESTEMATION_H
+#include <opencv2/core/mat.hpp>
+#include <functional>
+#include <opencv2/aruco.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/highgui.hpp>
+#include <unordered_map>
 
-void estimateMarkersPose(const cv::Mat frame, const cv::Mat distortionCoefficients, const cv::Mat cameraMatrix, std::vector<int>& markerIds, std::vector<cv::Vec3d> &rotationVectors, std::vector<cv::Vec3d> &translationVectors);
-void drawMarkersOnFrame(const cv::Mat &frame, const cv::Mat &distanceCoefficients, const cv::Mat &cameraMatrix,
-                        const std::vector<int> &markerIds, const std::vector<cv::Vec3d> &rotationVectors,
-                        const std::vector<cv::Vec3d> &translationVectors);
-void startCamMonitoring(std::string cameraCalibrationPath);
+
+// class AverageFilter{
+// public:
+//     AverageFilter();
+//     // AverageFilter(int bufferSize);
+//     void add(cv::Mat pose);
+//     cv::Mat getAverage();
+
+// private:
+//     int bufferSize;
+//     std::vector<cv::Mat> buffer;
+//     cv::Mat average;
+// };
+
+
+class AverageFilter{
+public:
+    AverageFilter();
+    void add(cv::Mat vec);
+    cv::Mat getAverage();
+
+private:
+    int mostFrequentElement(std::vector<double>);
+    int bufferSize;
+    std::vector<cv::Mat> buffer;
+    std::vector<double> valBuffer;
+    cv::Mat average;
+};
+
+class ArucoScanner{
+    public:
+        typedef cv::Mat vecs3d;
+        // typedef std::vector<cv::Vec3d> vecs3d;
+        // typedef std::unordered_map<int, cv::Vec3d> vecs3d;
+        ArucoScanner();
+        void estimateMarkersPose(cv::Mat frame);
+        void monitorArucoMarkers();
+        vecs3d getTranslationVectors();
+        vecs3d getRotationVectors();
+
+    private:
+        vecs3d translationVectors;
+        vecs3d rotationVectors;
+        cv::Mat cameraMatrix, distortionCoefficients;
+        cv::aruco::Dictionary markerDictionary;
+        cv::aruco::DetectorParameters detectorParams;
+        cv::aruco::ArucoDetector arucoDetector;
+        std::vector<std::vector<cv::Point2f>> markerCorners;
+        std::vector<int> markerIds;
+        std::vector<int> oldDetectedMarker;
+        AverageFilter rotVecAvg;
+        AverageFilter tranVecAvg;
+        int idsVectorSize = 0;
+
+        void showTransformationInfo(const cv::Mat &frame);
+        void openCamera(std::function<void(cv::Mat&)> func);
+        void drawArucoMarker(cv::Mat& frame);
+        void showCamErrorMassage();
+        std::function<void(cv::Mat&)> arucoMarkerDrawProccess = [=](cv::Mat& frame) {
+            estimateMarkersPose(frame);
+            drawArucoMarker(frame);
+        };
+        
+};
 
 #endif // SDP_ROBOTVISION_ARUCOPOSEESTEMATION_H

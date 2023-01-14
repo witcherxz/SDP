360e1ea822eb78a26fb470d87c2855f7c7398457
Abdullah <witcherxzr@hotmail.com>
2023-01-14 22:52:26 +0300

    change the structure of the cmake project

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include "iostream"
+#include <opencv2/core/mat.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "RobotVision/cameraCalibration/camera_calibration.h"
+#include "RobotVision/aruco_scanner.h"
+#include "RobotVision/Map/map.h"
+#include "RobotVision/Map/map_creator.h"
+#include "RobotVision/Map/map_viewer.h"
+#include "RobotVision/Map/a_star.cpp"
+#include "RobotVision/aruco_gnerator.h"
+#include "RobotVision/opencv_constants.h"
+
+int main(int argc, char **argv) {
+  // startPosCollection();
+  //  startCameraCalibration();
+  ArucoScanner as = ArucoScanner();
+  as.monitorArucoMarkers();
+  // CameraCenterCalibration cc = CameraCenterCalibration();
+  // cc.centerCalibration();
+  return 0;
+}

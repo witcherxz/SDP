657d815f201b2f5259ea7af6053f572fb3d532b6
Abdullah Khamis <witcherxz@hotmail.com>
2023-03-19 00:56:53 +0300

    Restructure the project

--- RobotVision/aruco_gnerator.h
+++
@@ -1,9 +0,0 @@
-#include <opencv2/aruco.hpp>
-
-#if !defined(SDP_ROBOTVISION_ARUCOMARKERS_H)
-#define SDP_ROBOTVISION_ARUCOMARKERS_H
-
-
-void createArucoMarkers(cv::aruco::PREDEFINED_DICTIONARY_NAME name, std::string path, int numberOfGeneration = 50);
-
-#endif // SDP_ROBOTVISION_ARUCOMARKERS_H
